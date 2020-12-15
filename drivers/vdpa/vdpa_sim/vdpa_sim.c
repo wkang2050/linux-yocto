8f1c15b7ac34a9a711de8cc2cadd4618822d28f1
Stefano Garzarella <sgarzare@redhat.com>
2020-12-15 15:42:43 +0100

    vdpa_sim: rename vdpasim_config_ops variables
    commit 36a9c30630256629e62a9186793c28735ade3ffc from
    https://github.com/altera-opensource/linux-socfpga.git
    
    These variables store generic callbacks used by the vDPA simulator
    core, so we can remove the 'net' word in their names.
    
    Co-developed-by: Max Gurtovoy <mgurtovoy@nvidia.com>
    Signed-off-by: Max Gurtovoy <mgurtovoy@nvidia.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    Signed-off-by: Stefano Garzarella <sgarzare@redhat.com>
    Link: https://lore.kernel.org/r/20201215144256.155342-6-sgarzare@redhat.com
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Wenlin Kang <wenlin.kang@windriver.com>

--- drivers/vdpa/vdpa_sim/vdpa_sim.c
+++ drivers/vdpa/vdpa_sim/vdpa_sim.c
@@ -354,8 +354,8 @@ static const struct dma_map_ops vdpasim_dma_ops = {
 	.free = vdpasim_free_coherent,
 };
 
-static const struct vdpa_config_ops vdpasim_net_config_ops;
-static const struct vdpa_config_ops vdpasim_net_batch_config_ops;
+static const struct vdpa_config_ops vdpasim_config_ops;
+static const struct vdpa_config_ops vdpasim_batch_config_ops;
 
 static struct vdpasim *vdpasim_create(struct vdpasim_dev_attr *dev_attr)
 {
@@ -365,9 +365,9 @@ static struct vdpasim *vdpasim_create(struct vdpasim_dev_attr *dev_attr)
 	int i, ret = -ENOMEM;
 
 	if (batch_mapping)
-		ops = &vdpasim_net_batch_config_ops;
+		ops = &vdpasim_batch_config_ops;
 	else
-		ops = &vdpasim_net_config_ops;
+		ops = &vdpasim_config_ops;
 
 	vdpasim = vdpa_alloc_device(struct vdpasim, vdpa, NULL, ops,
 				    dev_attr->nvqs);
@@ -685,7 +685,7 @@ static void vdpasim_free(struct vdpa_device *vdpa)
 	kfree(vdpasim->config);
 }
 
-static const struct vdpa_config_ops vdpasim_net_config_ops = {
+static const struct vdpa_config_ops vdpasim_config_ops = {
 	.set_vq_address         = vdpasim_set_vq_address,
 	.set_vq_num             = vdpasim_set_vq_num,
 	.kick_vq                = vdpasim_kick_vq,
@@ -712,7 +712,7 @@ static const struct vdpa_config_ops vdpasim_net_config_ops = {
 	.free                   = vdpasim_free,
 };
 
-static const struct vdpa_config_ops vdpasim_net_batch_config_ops = {
+static const struct vdpa_config_ops vdpasim_batch_config_ops = {
 	.set_vq_address         = vdpasim_set_vq_address,
 	.set_vq_num             = vdpasim_set_vq_num,
 	.kick_vq                = vdpasim_kick_vq,
