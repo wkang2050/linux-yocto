6d538e76fc8941e829eb7ea5a8869f0c04ac42bc
Aaron Tomlin <atomlin@redhat.com>
2022-09-28 15:47:29 +0530

    mm/vmstat: Add CPU-specific variable to track a vmstat discrepancy
    commit 371fbdb877ba8b753034aee7d1f731cffc0adec0 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Introduce a CPU-specific variable namely vmstat_dirty to indicate
    if a vmstat imbalance is present for a given CPU. Therefore, at
    the appropriate time, we can fold all the remaining differentials.
    This patch also provides trivial helpers for modification and testing.
    
    Signed-off-by: Aaron Tomlin <atomlin@redhat.com>
    Signed-off-by: Bharat Bhushan <bbhushan2@marvell.com>
    [Fix backporting issues from upstream]
    
    Change-Id: Icee6e2616b65a2cd21f2ed911354f9f31cc9d400
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/87318
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Wenlin Kang <wenlin.kang@windriver.com>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -168,6 +168,22 @@ EXPORT_SYMBOL(vm_numa_stat);
 EXPORT_SYMBOL(vm_node_stat);
 
 #ifdef CONFIG_SMP
+static DEFINE_PER_CPU_ALIGNED(bool, vmstat_dirty);
+
+static inline void vmstat_mark_dirty(void)
+{
+	this_cpu_write(vmstat_dirty, true);
+}
+
+static inline void vmstat_clear_dirty(void)
+{
+	this_cpu_write(vmstat_dirty, false);
+}
+
+static inline bool is_vmstat_dirty(void)
+{
+	return this_cpu_read(vmstat_dirty);
+}
 
 int calculate_pressure_threshold(struct zone *zone)
 {
